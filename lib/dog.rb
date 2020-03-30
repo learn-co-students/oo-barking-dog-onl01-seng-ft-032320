bae34a667f4f4ce5341873f43628f9b972d374b8
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-30 14:11:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name) #setter
+    @this_dogs_name = dog_name
+  end 
+  
+  def name #getter 
+    @this_dogs_name
+  end 
+  
+  def bark
+    puts "woof!"
+  end 
+end 
